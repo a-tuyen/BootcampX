b33735791b2d3981816026ad30f9af8d3718a913
Amanda Tuyen <atuyen26@gmail.com>
2021-04-01 04:32:13 +0000

    changed typo in table name

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -7,12 +7,14 @@ CREATE TABLE assignments (
   duration INTEGER
 );
 
-CREATE TABLE assingment_submissions (
+CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
 );
+
+
 
 
